d848b349fd38088f29a133b69aab9f9fe02fe9ff
Banano <banan@gmail.com>
2016-03-06 03:04:39 +0300

    fix initialize projects and add simple class

---
+++ modules/core/source/main/java/org/itsystem/modules/core/entity/ItObject.java
@@ -0,0 +1,10 @@
+package org.itsystem.modules.core.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class ItObject implements Serializable {
+
+}
